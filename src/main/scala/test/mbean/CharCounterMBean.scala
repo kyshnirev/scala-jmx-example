6054307fcbe73d4ad9e15fbbedb522dbe9ebeca9
G.Kushniryov <gkushniryov@ac-sw.com>
2016-08-18 19:43:20 +0700

    MBean editable field "filter"

--- src/main/scala/test/mbean/CharCounterMBean.scala
+++ src/main/scala/test/mbean/CharCounterMBean.scala
@@ -4,6 +4,9 @@ import scala.collection.mutable
 
 // define an "interface" (name ends with "MBean"!)
 trait CharCounterMBean {
+  protected var filter : Set[Char] = Set.empty
+  def getFilter : String = filter.mkString(", ")
+  def setFilter(commaSplitChars : String) = filter = commaSplitChars.toCharArray.filter { c => c != ' ' && c != ',' }.toSet
   def getAllChars : String
   def getCharsCount : Array[String]
   def clear : Unit
@@ -14,6 +17,7 @@ class CharCounter extends CharCounterMBean {
   private val cmap = new mutable.HashMap[Char, Int].withDefaultValue(0)
 
   def putChar(c : Char) = {
+    if (filter.isEmpty || filter.contains(c))
       cmap(c) += 1
   }
 
