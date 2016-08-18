2e3e75dd9cf9e4632c9210de49f5ecb19da42149
G.Kushniryov <gkushniryov@ac-sw.com>
2016-08-18 19:16:43 +0700

    operation "clear" - remove all keys form CharCounter

--- src/main/scala/test/mbean/CharCounterMBean.scala
+++ src/main/scala/test/mbean/CharCounterMBean.scala
@@ -1,14 +1,17 @@
 package test.mbean
 
+import scala.collection.mutable
+
 // define an "interface" (name ends with "MBean"!)
 trait CharCounterMBean {
   def getAllChars : String
   def getCharsCount : Array[String]
+  def clear : Unit
 }
 
 class CharCounter extends CharCounterMBean {
 
-  private val cmap = new scala.collection.mutable.HashMap[Char, Int].withDefaultValue(0)
+  private val cmap = new mutable.HashMap[Char, Int].withDefaultValue(0)
 
   def putChar(c : Char) = {
       cmap(c) += 1
@@ -22,5 +25,7 @@ class CharCounter extends CharCounterMBean {
           .sortBy(_._1)
           .map { case (k, v) => s"$k = $v" }
           .toArray
+
+   override def clear = cmap.clear()
 
 }
